f9cddf1893e1e44996b9d1e7d75819c695397d6b
GeQi <geqi2012@gmail.com>
2015-02-14 22:50:06 -0700

    添加下载列表
    删除编译相关
    整理代码

--- PixivAgent.py
+++ PixivAgent.py
@@ -5,15 +5,17 @@ import re
 import sys
 import threading
 import Queue
+import shutil
+from zipfile import ZipFile
 
 import requests
 from lxml import html
+from PIL import Image
+import images2gif
 
 import ui_PixivAgent
-import ui_PixivAgent_login
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
 
 def get_page(session, url):
     request = session.get(url)
@@ -22,14 +24,15 @@ def get_page(session, url):
     return doc
 
 def iter_urls_work(session, id_user, type, num):
-    for page in range(num / 20 + 1):
+    for page in range(num//20+1):
         url = "http://www.pixiv.net/member_illust.php?id=%d&type=%s&p=%d" % (id_user, type, page+1)
         elems_work = get_page(session, url).find_class("image-item")
 
         for i, elem_work in enumerate(elems_work):
-            yield elem_work.find("a").get("href")
-            if (page * 20 + i + 1) >= num:
-                break
+            if page*20+i+1 <= num:
+                yield elem_work.find("a").get("href")
+            else:
+                raise StopIteration
 
 class Work(object):
     def __init__(self, session, url_work):
@@ -47,7 +50,7 @@ class Work(object):
 
     def get_type(self):
         elem_works_display = self.page.find_class("works_display")
-        if not elem_works_display:
+        if elem_works_display[0].find_class("player"):
             return "ugoira"
         elif elem_works_display[0].find_class("manga"):
             return "manga"
@@ -63,7 +66,10 @@ class Work(object):
         return self.page.find_class("work-info")[0].find_class("title")[0].text
 
     def get_urls_image(self):
-        if self.type == "illust":
+        if self.type == "ugoira":
+            url = re.findall(r'http\S*?1080\.zip', html.tostring(self.page))
+            return url[0].replace("\\", "")
+        elif self.type == "illust":
             elems_image = self.page.find_class("original-image")
         elif self.type == "multiple":
             url = self.page.find_class("works_display")[0].find("a").get("href")
@@ -71,42 +77,64 @@ class Work(object):
         elif self.type == "manga":
             url = self.page.find_class("works_display")[0].find("a").get("href")
             elems_image = get_page(self.session, url).find_class("image")
-        elif self.type == "ugoira":
-            raise NotImplementedError        # TODO
-
         return [elem_image.get("data-src") for elem_image in elems_image]
 
     def download(self, dir):
-        dir_download = os.path.join(dir, self.title+" "+self.id)
-        try:
+        dir_name = self.title+" "+self.id
+        escaped = re.sub(r'[/\\:*?"<>|]', '-', dir_name)
+        dir_download = os.path.join(dir, escaped)
+        if not os.path.exists(dir_download):
             os.makedirs(dir_download)
-        except Exception:
-            raise IOError
 
-        for i, url in enumerate(self.urls_image):
-            request_image = self.session.get(url)
-            if self.type == "illust":
-                with open(os.path.join(dir_download, self.id+".jpg"), "wb") as file:
-                    file.write(request_image.content)
-            elif self.type == "multiple" or "manga":
-                with open(os.path.join(dir_download, self.id+"_"+str(i+1)+r".jpg"), "wb") as file:
-                    file.write(request_image.content)
-            elif self.type == "ugoira":
-                raise NotImplementedError        # TODO
+        if self.type == "ugoira":
+            dir_temp = os.path.join(dir_download, "temp")
+            dir_zip = os.path.join(dir_temp, self.id+".zip")
+            frames = []
+            os.makedirs(dir_temp)
+            url = self.urls_image
+            request_zip = self.session.get(url)
+
+            with open(dir_zip, "wb") as file:
+                file.write(request_zip.content)
+
+            with ZipFile(dir_zip) as file_zip:
+                for image in file_zip.namelist():
+                    file_zip.extract(image, dir_temp)
+                    frames.append(Image.open(os.path.join(dir_temp, image)))
+
+            images2gif.writeGif(os.path.join(dir_download, self.id+'.gif'), frames, duration=0.1)
+            shutil.rmtree(dir_temp)
+        else:
+            for i, url in enumerate(self.urls_image):
+                request_image = self.session.get(url)
+                if self.type == "illust":
+                    with open(os.path.join(dir_download, self.id+".jpg"), "wb") as file:
+                        file.write(request_image.content)
+                elif self.type == "multiple" or "manga":
+                    with open(os.path.join(dir_download, self.id+"_"+str(i+1)+r".jpg"), "wb") as file:
+                        file.write(request_image.content)
 
 
 class Main(QDialog, ui_PixivAgent.Ui_main):
     # signals:
-    signal_analyse_finished = pyqtSignal()
+    signal_login = pyqtSignal(bool)
+
+    signal_analyse_start = pyqtSignal()
+    signal_analyse = pyqtSignal(bool)
 
     def __init__(self):
         QDialog.__init__(self)
         ui_PixivAgent.Ui_main.__init__(self)
         self.setupUi(self)
-        self.set_all(False, btn=True)
+        self.set_login_mode(True)
         self.dir.setText(os.path.join(os.getcwd(), "Download"))
 
-        # 建立会话
+        # 初始化下载列表gui
+        self.hide_table()
+        self.table.setSelectionBehavior(QTableWidget.SelectRows)
+        self.table.setEditTriggers(QTableWidget.NoEditTriggers)
+
+        # 初始化会话
         self.session = requests.Session()
         headers = {"User-Agent": r"Chrome/40.0.2214.93",
                    "Referer": r"http://www.pixiv.net/"}
@@ -114,7 +142,6 @@ class Main(QDialog, ui_PixivAgent.Ui_main):
 
         # 初始化队列
         self.queue = Queue.Queue()
-        self.history = []
 
         # 初始化线程
         self.threads_num = 5
@@ -125,99 +152,26 @@ class Main(QDialog, ui_PixivAgent.Ui_main):
         self.create_thread_download()
 
         # connections
-        self.btn.clicked.connect(self.show_login)
-        self.btn_dir.clicked.connect(self.show_dir)
-        self.signal_analyse_finished.connect(self.event_download.set)
-        self.signal_analyse_finished.connect(self.set_all)
-
-    def set_all(self, bool=True, btn=True):
-        self.id.setEnabled(bool)
-        self.amount.setEnabled(bool)
-        self.dir.setEnabled(bool)
-        self.btn_dir.setEnabled(bool)
-        self.btn.setEnabled(btn)
-
-    # slots
-    def create_thread_analyse(self):
-
-        def thread_analyse():
-            while True:
-                self.event_analyse.wait()
-                self.set_all(False, btn=False)
-                iter = iter_urls_work(self.session, int(self.id.text()), "", int(self.amount.text()))
-                for _url in iter:
-                    work = Work(self.session, _url)
-                    with self.lock:
-                        if work not in self.history:
-                            self.history.append(work)
-                            self.queue.put(work)
-                self.signal_analyse_finished.emit()
-                self.event_analyse.clear()
-
-        thread = threading.Thread(target=thread_analyse)
-        thread.setDaemon(True)
-        thread.start()
-
-    def create_thread_download(self):
-        def thread_download():
-            while True:
-                self.event_download.wait()
-                print "thread awoke"
-                while not self.queue.empty():
-                    with self.lock:
-                        work = self.queue.get()
-                    work.download(str(self.dir.text()))
-                self.event_download.clear()
-
-        self.threads_download = [threading.Thread(target=thread_download) for i in range(self.threads_num)]
-        for _thread in self.threads_download:
-            _thread.setDaemon(True)
-            _thread.start()
-
-    def show_login(self):
-        self.login = Login(self.session, self.unlock)
-        self.login.show()
-
-    def show_dir(self):
-        dir = QFileDialog.getExistingDirectory(self, u"选择下载目录")
-        self.dir.setText(dir)
-
-    def unlock(self):
-        self.set_all(True)
-        self.btn.setText(u"下载")
-
-        self.btn.clicked.disconnect()
-        self.btn.clicked.connect(self.event_analyse.set)
-
-
-class Login(QDialog, ui_PixivAgent_login.Ui_login):
-    # signals
-    signal_login_successed = pyqtSignal()
-    signal_login_failed = pyqtSignal()
-
-    def __init__(self, session, slot):
-        QDialog.__init__(self)
-        ui_PixivAgent_login.Ui_login.__init__(self)
-        self.setupUi(self)
-
-        self.session = session
-
-        # connections
         self.btn_login.clicked.connect(self.login)
-        self.signal_login_successed.connect(slot)
-        self.signal_login_successed.connect(self.close)
-        self.signal_login_failed.connect(self.set_all)
+        self.signal_login.connect(self.check_login)
 
-    def set_all(self, bool=True):
-        self.btn_login.setEnabled(bool)
-        self.email.setReadOnly(not bool)
-        self.password.setReadOnly(not bool)
+        self.btn_dir.clicked.connect(self.show_dir)
 
+        self.btn_analyse.clicked.connect(self.event_analyse.set)
+        self.signal_analyse_start.connect(self.analyse_start)
+        self.signal_analyse.connect(self.check_analyse)
+
+    # 操作gui
+    def set_login_mode(self, bool):
+        self.widget_login.setVisible(bool)
+        self.widget_analyse.setVisible(not bool)
+
+    # 登录线程
     def login(self):
-        self.set_all(False)
+        self.enable_login_input(False)
 
         # 登录
-        def post():
+        def thread_login():
             url_login = r"https://www.secure.pixiv.net/login.php"
             data = {"mode": "login",
                     "pixiv_id": str(self.email.text()),
@@ -226,12 +180,114 @@ class Login(QDialog, ui_PixivAgent_login.Ui_login):
 
             # 确认
             if request_login.status_code != 302:
-                self.signal_login_failed.emit()
+                self.signal_login.emit(False)
             else:
-                self.signal_login_successed.emit()
+                self.signal_login.emit(True)
 
-        thread = threading.Thread(target=post)
-        thread.start()
+        self.thread_login = threading.Thread(target=thread_login)
+        self.thread_login.setDaemon(True)
+        self.thread_login.start()
+
+    # 操作gui
+    def enable_login_input(self, bool):
+        self.email.setEnabled(bool)
+        self.password.setEnabled(bool)
+        self.btn_login.setEnabled(bool)
+
+    def check_login(self, bool):
+        if bool:
+            self.set_login_mode(False)
+        else:
+            self.enable_login_input(True)
+
+    # 解析线程
+    def create_thread_analyse(self):
+        def thread_analyse():
+            while True:
+                self.event_analyse.wait()
+                self.signal_analyse_start.emit()
+                print "working"
+                iter = iter_urls_work(self.session, int(self.id.text()), "", int(self.amount.text()))
+                print "1"
+                for url in iter:
+                    work = Work(self.session, url)
+                    print "2"
+                    with self.lock:
+                        self.queue.put(work)
+                        print "3"
+                print "job done"
+                self.signal_analyse.emit(True)
+                self.event_analyse.clear()
+
+        self.thread_analyse = threading.Thread(target=thread_analyse)
+        self.thread_analyse.setDaemon(True)
+        self.thread_analyse.start()
+
+    # 操作gui
+    def show_table(self):
+        self.btn_table.clicked.disconnect()
+        self.btn_table.setText(u"收起下载列表")
+        self.btn_table.clicked.connect(self.hide_table)
+        self.table.setVisible(True)
+        self.setMinimumHeight(297)
+        self.setMaximumHeight(10800)
+
+    def hide_table(self):
+        self.btn_table.setText(u"展开下载列表")
+        self.btn_table.clicked.connect(self.show_table)
+        self.table.setVisible(False)
+        self.setMinimumHeight(99)
+        self.setMaximumHeight(99)
+
+    def enable_analyse_input(self, bool):
+        self.id.setEnabled(bool)
+        self.amount.setEnabled(bool)
+        self.dir.setEnabled(bool)
+        self.btn_dir.setEnabled(bool)
+        self.btn_analyse.setEnabled(bool)
+
+    def analyse_start(self):
+        self.enable_analyse_input(False)
+        self.show_table()
+
+    def check_analyse(self, bool):
+        if bool:
+            self.event_download.set()
+            self.enable_analyse_input(True)
+        else:
+            pass     # TODO: 报错
+
+    # 下载线程
+    def create_thread_download(self):
+        def thread_download():
+            while True:
+                self.event_download.wait()
+                while True:
+                    with self.lock:
+                        if not self.queue.empty():
+                            work = self.queue.get()
+                        else:
+                            break
+                    work.download(str(self.dir.text()))
+                self.event_download.clear()
+
+        self.threads_download = [threading.Thread(target=thread_download) for i in range(self.threads_num)]
+        for thread in self.threads_download:
+            thread.setDaemon(True)
+            thread.start()
+
+    # 目录选择窗口
+    def show_dir(self):
+        dir_download = QFileDialog.getExistingDirectory(self, u"选择下载目录")
+        self.dir.setText(dir_download)
+
+
+#     def test(self):
+#         self.table.insertRow(0)
+#         progress = QProgressBar()
+#         progress.setAlignment(Qt.AlignHCenter)
+#         progress.setValue(100)
+#         self.table.setCellWidget(0, 2, progress)
 
 
 app = QApplication(sys.argv)
--- uic.bat
+++ uic.bat
@@ -1,1 +1,1 @@
-pyuic4 -x PixivAgent.ui -o ui_PixivAgent.py & pyuic4 -x PixivAgent_login.ui -o ui_PixivAgent_login.py
+pyuic4 -x PixivAgent.ui -o ui_PixivAgent.py
